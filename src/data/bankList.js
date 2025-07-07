3ea47ee3c261a3651865774745b00a9f84067fb1
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-07 06:43:42 +0530

    add bank logos and create bankList data structure

---
+++ src/data/bankList.js
@@ -0,0 +1,16 @@
+export const bankList = [
+  { id: 1, name: "State Bank of India", logo: "/src/assets/bankLogos/sbiLogo.webp" },
+  { id: 2, name: "Axis Bank", logo: "/src/assets/bankLogos/axisLogo.webp" },
+  { id: 3, name: "HDFC Bank", logo: "/src/assets/bankLogos/hdfcLogo.webp" },
+  { id: 4, name: "ICICI Bank", logo: "/src/assets/bankLogos/iciciLogo.webp" },
+  { id: 5, name: "Punjab National Bank", logo: "/src/assets/bankLogos/pnbLogo.webp" },
+  { id: 6, name: "Bank of Baroda", logo: "/src/assets/bankLogos/barodaLogo.webp" },
+  { id: 7, name: "ESAF Bank", logo: "/src/assets/bankLogos/esafLogo.webp" },
+  { id: 8, name: "Federal Bank", logo: "/src/assets/bankLogos/fedLogo.webp" },
+  { id: 9, name: "CSB Bank", logo: "/src/assets/bankLogos/csbLogo.webp" },
+  { id: 10, name: "Karnataka Bank", logo: "/src/assets/bankLogos/karnatakaLogo.webp" },
+  { id: 11, name: "Kotak Mahindra Bank", logo: "/src/assets/bankLogos/kotakLogo.webp" },
+  { id: 12, name: "South Indian Bank", logo: "/src/assets/bankLogos/southIndiaLogo.webp" },
+  { id: 13, name: "Union Bank of India", logo: "/src/assets/bankLogos/unionLogo.webp" },
+  { id: 14, name: "Yes Bank", logo: "/src/assets/bankLogos/yesLogo.webp" },
+];
