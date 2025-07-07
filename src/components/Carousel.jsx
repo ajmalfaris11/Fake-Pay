cb4ee35e3948ac770448d03cc8a5f5c09e4fb2ae
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-07 18:37:33 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "@material-tailwind/react";
 
 import buildTrust from "../assets/Carousel/buildTrust.webp";
 import increaseSales from "../assets/Carousel/increaseSales.webp";
- 
+
 export function CarouselCustomNavigation() {
   return (
     <Carousel
@@ -11,15 +11,14 @@ export function CarouselCustomNavigation() {
       autoplayDelay={5000}
       loop={true}
       prevArrow={() => null}
-  nextArrow={() => null}
+      nextArrow={() => null}
       navigation={({ setActiveIndex, activeIndex, length }) => (
         <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
           {new Array(length).fill("").map((_, i) => (
             <span
               key={i}
-              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-              }`}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                }`}
               onClick={() => setActiveIndex(i)}
             />
           ))}
