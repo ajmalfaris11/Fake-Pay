cb110973cc6fe3fe631f4821a8ce29af47a15f4a
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-07 16:24:31 +0530

    fix: remove unused InputForm import in InputPage component

--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import NavBar from "../components/NavBar";
-import InputForm from "../components/InputForm";
 import { useParams, useNavigate } from "react-router-dom";
 import { bankList } from "../data/bankList";
 
