4698c492114934a14f51f1fdef4b64cd0299a55b
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-07 21:24:10 +0530

    feat: use remote icon URL for PWA manifest and fix manifest config

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: '/icons/icon-192.png',
+            src: 'https://cdn3.iconfinder.com/data/icons/shopping-and-ecommerce-29/90/secure_payment-512.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512.png',
+            src: 'https://cdn3.iconfinder.com/data/icons/shopping-and-ecommerce-29/90/secure_payment-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
