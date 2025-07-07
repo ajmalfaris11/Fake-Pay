6340d2108dc2f06c09f9c8016ebd49cd3acdb000
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-07 18:43:06 +0530

    Used ES module imports for each logo for versel production

--- src/data/bankList.js
+++ src/data/bankList.js
@@ -1,86 +1,101 @@
+import sbiLogo from "../assets/bankLogos/sbiLogo.webp";
+import axisLogo from "../assets/bankLogos/axisLogo.webp";
+import hdfcLogo from "../assets/bankLogos/hdfcLogo.webp";
+import iciciLogo from "../assets/bankLogos/iciciLogo.webp";
+import pnbLogo from "../assets/bankLogos/pnbLogo.webp";
+import barodaLogo from "../assets/bankLogos/barodaLogo.webp";
+import esafLogo from "../assets/bankLogos/esafLogo.webp";
+import fedLogo from "../assets/bankLogos/fedLogo.webp";
+import csbLogo from "../assets/bankLogos/csbLogo.webp";
+import karnatakaLogo from "../assets/bankLogos/karnatakaLogo.webp";
+import kotakLogo from "../assets/bankLogos/kotakLogo.webp";
+import southIndiaLogo from "../assets/bankLogos/southIndiaLogo.webp";
+import unionLogo from "../assets/bankLogos/unionLogo.webp";
+import yesLogo from "../assets/bankLogos/yesLogo.webp";
+
 export const bankList = [
   {
     id: 1,
     name: "State Bank of India",
-    logo: "/src/assets/bankLogos/sbiLogo.webp",
-    upiSuffix: "@oksbi"
+    logo: sbiLogo,
+    upiSuffix: "@oksbi",
   },
   {
     id: 2,
     name: "Axis Bank",
-    logo: "/src/assets/bankLogos/axisLogo.webp",
-    upiSuffix: "@okaxis"
+    logo: axisLogo,
+    upiSuffix: "@okaxis",
   },
   {
     id: 3,
     name: "HDFC Bank",
-    logo: "/src/assets/bankLogos/hdfcLogo.webp",
-    upiSuffix: "@okhdfcbank"
+    logo: hdfcLogo,
+    upiSuffix: "@okhdfcbank",
   },
   {
     id: 4,
     name: "ICICI Bank",
-    logo: "/src/assets/bankLogos/iciciLogo.webp",
-    upiSuffix: "@okicici"
+    logo: iciciLogo,
+    upiSuffix: "@okicici",
   },
   {
     id: 5,
     name: "Punjab National Bank",
-    logo: "/src/assets/bankLogos/pnbLogo.webp",
-    upiSuffix: "@okpnb"
+    logo: pnbLogo,
+    upiSuffix: "@okpnb",
   },
   {
     id: 6,
     name: "Bank of Baroda",
-    logo: "/src/assets/bankLogos/barodaLogo.webp",
-    upiSuffix: "@okbaroda"
+    logo: barodaLogo,
+    upiSuffix: "@okbaroda",
   },
   {
     id: 7,
     name: "ESAF Bank",
-    logo: "/src/assets/bankLogos/esafLogo.webp",
-    upiSuffix: "@okesaf"
+    logo: esafLogo,
+    upiSuffix: "@okesaf",
   },
   {
     id: 8,
     name: "Federal Bank",
-    logo: "/src/assets/bankLogos/fedLogo.webp",
-    upiSuffix: "@okfederal"
+    logo: fedLogo,
+    upiSuffix: "@okfederal",
   },
   {
     id: 9,
     name: "CSB Bank",
-    logo: "/src/assets/bankLogos/csbLogo.webp",
-    upiSuffix: "@okcsb"
+    logo: csbLogo,
+    upiSuffix: "@okcsb",
   },
   {
     id: 10,
     name: "Karnataka Bank",
-    logo: "/src/assets/bankLogos/karnatakaLogo.webp",
-    upiSuffix: "@okktkbank"
+    logo: karnatakaLogo,
+    upiSuffix: "@okktkbank",
   },
   {
     id: 11,
     name: "Kotak Mahindra Bank",
-    logo: "/src/assets/bankLogos/kotakLogo.webp",
-    upiSuffix: "@okkotak"
+    logo: kotakLogo,
+    upiSuffix: "@okkotak",
   },
   {
     id: 12,
     name: "South Indian Bank",
-    logo: "/src/assets/bankLogos/southIndiaLogo.webp",
-    upiSuffix: "@oksib"
+    logo: southIndiaLogo,
+    upiSuffix: "@oksib",
   },
   {
     id: 13,
     name: "Union Bank of India",
-    logo: "/src/assets/bankLogos/unionLogo.webp",
-    upiSuffix: "@okunionbank"
+    logo: unionLogo,
+    upiSuffix: "@okunionbank",
   },
   {
     id: 14,
     name: "Yes Bank",
-    logo: "/src/assets/bankLogos/yesLogo.webp",
-    upiSuffix: "@okyesbank"
-  }
+    logo: yesLogo,
+    upiSuffix: "@okyesbank",
+  },
 ];
