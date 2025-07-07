a600b16d72ef3318c312879a6aa0d471fb31409e
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-07 15:40:42 +0530

    feat: add platform identifiers to payment options and integrate CarouselCustomNavigation in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,26 +2,30 @@
 import NavBar from "../components/NavBar";
 import PlatformCard from "../components/PlatformCard";
 import { href, useNavigate } from "react-router-dom";
+import {CarouselCustomNavigation} from "../components/Carousel";
 
 export default function HomePage() {
   const navigate = useNavigate();
 
   const platforms = [
-    { name: "google pay", description: "create with Google Pay", logo:"src/assets/gpayIcon.webp" },
-    { name: "PhonePay", description: "create with PhonePe", logo:"src/assets/phonepayIcon.webp" },
-    { name: "Paytm", description: "create with Paytm", logo:"src/assets/paytmIcon.webp" },
-    { name: "Amazon Pay", description: "create with Amazon Pay", logo:"src/assets/amazonePayIcon.webp" },
+    { name: "google pay", description: "create with Google Pay", logo:"src/assets/gpayIcon.webp", platform: "gpay" },
+    { name: "PhonePay", description: "create with PhonePe", logo:"src/assets/phonepayIcon.webp", platform: "phonepe" },
+    { name: "Paytm", description: "create with Paytm", logo:"src/assets/paytmIcon.webp", platform: "paytm" },
+    { name: "Amazon Pay", description: "create with Amazon Pay", logo:"src/assets/amazonePayIcon.webp", platform: "amazonpay" },
   ];
 
   return (
     <div>
       <NavBar />
-      <div className="p-4 grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="p-2">
+        <CarouselCustomNavigation/>
+      </div>
+      <div className="p-4 grid grid-cols-2 md:grid-cols-4 gap-4 mt-10 md:mt-20">
         {platforms.map((platform, idx) => (
           <PlatformCard 
             key={idx}
             platform={platform}
-            onClick={() => navigate(`/input/${platform.name.toLowerCase()}`)}
+            onClick={() => navigate(`/input/${platform.platform.toLowerCase()}`)}
           />
         ))}
       </div>
