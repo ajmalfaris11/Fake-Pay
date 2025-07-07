f369a5cf845ddb662cc9d934ed9b9704529bbe1b
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-07 17:42:55 +0530

    fix: correct logo import and update class attribute in NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
-// src/components/NavBar.jsx
+import logo from "../assets/fakePayLogo.webp";
+
 export default function NavBar() {
   return (
     <nav className="bg-[#1e40af] p-2 text-white text-center w-full flex justify-between items-center fixed top-0 z-50 px-4">
-      <span class="material-symbols-outlined">
+      <span className="material-symbols-outlined">
         sort
       </span>
-      <img src="/src/assets/fakePayLogo.webp" alt="fake pay log" className="w-36 md:w-52 mx-auto" />
+
+      <img src={logo} alt="Fake Pay Logo" className="w-36 md:w-52 mx-auto" />
 
       <span className="material-symbols-outlined text-2xl md:text-3xl">
         face_6
