31a6cce5ed2536a90f09b3470abac4bf5b722e0e
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-12 18:57:38 +0530

    formatDateTime utils helper added

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,17 @@
 export function amtFormate(amount) {
   return Number(amount || 0).toLocaleString('en-IN');
 }
+
+    function formatDateTime (dateTimeStr){
+        if (!dateTimeStr) return "";
+        const date = new Date(dateTimeStr);
+        return date.toLocaleString("en-GB", {
+            timeZone: "Asia/Kolkata",
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+        });
+    };
