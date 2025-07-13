24db17817744493d04567a558e56fc1e9df4f841
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-13 07:31:22 +0530

    screen short option page designed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/fakePayLogo.webp";
 
 export default function NavBar() {
   return (
-    <nav className="bg-[#1e40af] p-2 text-white text-center w-full flex justify-between items-center fixed top-0 z-50 px-4">
+    <nav className="bg-[#1e40af] p-4 text-white text-center w-full flex justify-between items-center fixed top-0 z-50 px-4">
       <span className="material-symbols-outlined">
         sort
       </span>
---
+++ src/pages/SelectScreenshotType.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavBar from '../components/NavBar';
+
+export default function SelectScreenshotType() {
+
+  return (
+    <div className="min-h-screen bg-white text-black flex flex-col items-center justify-center px-4 font-sans">
+      <NavBar />
+      <div className="text-center mb-14">
+        <h1 className="text-4xl font-extrabold uppercase text-center text-[#1e40af]">Select Screenshot Type</h1>
+        <p className="mb-8">Choose what you want to create.</p>
+      </div>
+
+      <button className="w-full bg-[#1e40af] text-white p-4 rounded-2xl mb-4 flex items-center justify-between text-xl font-bold">
+        <div className='w-20 h-20 rounded-2xl bg-white flex items-center justify-center'>
+          <span className="material-symbols-outlined text-[#1e40af] text-5xl">
+            check_circle
+          </span>
+        </div>
+        <p className='w-4/5'>Instant Success <br /> Page Screenshot</p>
+      </button>
+
+      <button className="w-full bg-[#1e40af] text-white p-4 rounded-2xl mb-4 flex items-center justify-between text-xl font-bold">
+        <div className='w-20 h-20 rounded-2xl bg-white flex items-center justify-center'>
+          <span className="material-symbols-outlined text-[#1e40af] text-5xl">
+            receipt
+          </span>
+        </div>
+        <p className='w-4/5'>Receipt Page Screenshot</p>
+      </button>
+    </div>
+  );
+}
+
+
